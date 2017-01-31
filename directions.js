9e9d1dd3c979595cebfcd1395427c754ab76f4e3
BenCovey <benvcovey@gmail.com>
2017-01-31 17:35:00 -0400

    Finished Project

--- directions.js
+++ directions.js
@@ -1,24 +1,28 @@
 var g_html;
 var origin;
 var destination;
+var time = 0;
+var hours = 0;
+var minutes = 0;
+var spinner = document.getElementById("loader");
 function displaydirections() {
     origin = document.getElementById("origin");
     destination = document.getElementById("destination");
     
     // Validation
     if (!origin.checkValidity()) {
-       alert("ERROR: Origin Field") 
+       alert("ERROR: Origin Field");
         return;
     }
     if (!destination.checkValidity()) {
-        alert("ERROR: Destination Field")
+        alert("ERROR: Destination Field");
         return;
     }
     origin = origin.value;
     destination = destination.value;
     var xhttp = new XMLHttpRequest();
     xhttp.onloadstart = function() {
-        spinner.style.display = "block";
+         var block = "block";
     }
     xhttp.onload = function() {
         if (this.status == 200) {
@@ -29,12 +33,9 @@ function displaydirections() {
         alert("Error loading direction data" + err);
     }
     xhttp.onloadend = function() {
-        spinner.style.display = "none";
     }
-    var porigin = document.getElementById('origindiv');
-    var pdestination = document.getElementById('destinationdiv');
-    porigin.innerHTML = "<div class= 'well well-sm'>" + "Leave " + origin + "</div>";
-    pdestination.innerHTML = "<div class= 'well well-sm'>" + "Arrive at " +  destination + "</div>";
+    
+    
     // Could display progress bar as data is loading.
     var yql = "https://maps.googleapis.com/maps/api/directions/xml?origin=" + origin +
         "&destination=" + destination + "&key=AIzaSyAQLrqyOK42M1juBuy9SY4DUVPdqnlVeEA";
@@ -48,8 +49,10 @@ function loadData(response) {
         alert('Invalid response from server.');
         return;
     }
+    
     var directions = xmlDoc.getElementsByTagName("step");
-    if (directions.length == 0) {
+    
+    if (directions.length === 0) {
         alert("No Direction Data.");
     }
     for (var i = 0; i < directions.length; i++) {
@@ -59,6 +62,31 @@ function loadData(response) {
             displayDirections(direc, i, directions.length);
         }
     }
+    try{
+        var realtime = Math.ceil(time/60);
+        console.log(parseInt(realtime));
+        minutes = Math.ceil(realtime);
+        if(minutes > 59){
+            hours = parseInt(minutes%60);
+            minutes = parseInt(minutes/hours);
+            
+        }
+    }catch(err) {
+        console.log("Error:" + err);
+        var porigin = document.getElementById('origindiv');
+        var pdestination = document.getElementById('destinationdiv');
+        porigin.innerHTML = "<div class= 'well well-sm'>" + "Leave " + origin + "</div>";
+        pdestination.innerHTML = "<div class= 'well well-sm'>" + "Arrive at " +  destination + "</div>";
+        }
+    
+    var porigin = document.getElementById('origindiv');
+    var pdestination = document.getElementById('destinationdiv');
+    if(hours > 0){
+        porigin.innerHTML = "<div class= 'well well-sm'>" + "Leave " + origin + " arrive in " + hours + " hours and " + minutes + " minutes</div>";
+    }else{
+        porigin.innerHTML = "<div class= 'well well-sm'>" + "Leave " + origin + " arrive in " + minutes + " minutes</div>";
+    }
+    pdestination.innerHTML = "<div class= 'well well-sm'>" + "Arrive at " +  destination + "</div>";
 
 }
 
@@ -71,17 +99,36 @@ function displayDirections(direc, num, maxnum) {
     var distance = direc.getElementsByTagName("distance")[0];
     var distsub = distance.getElementsByTagName("text")[0];
     var nodeDist = distsub.childNodes[0];
-    //Duration
+    //Time
     var duration = direc.getElementsByTagName("duration")[0];
+    var timesub = duration.getElementsByTagName("value")[0]
+    var nodeTime = timesub.childNodes[0];
+    time += parseInt(nodeTime.nodeValue);
+    //Duration
     var dursub = duration.getElementsByTagName("text")[0];
     var nodeDur = dursub.childNodes[0];
+    //Mode
+    var travelmde = direc.getElementsByTagName("travel_mode")[0];
+    var nodeMode = travelmde.childNodes[0];
+    text = (nodeMode.nodeValue);
+    text = getModeImage(text);
     if(g_html == null){
-        g_html = "<div class='well' id='step"+num+"'> " + nodeD.nodeValue +" "  +
+        g_html = "<div class='well' id='step"+num+"'>" + text  + " " + nodeD.nodeValue +" "  +
     nodeDist.nodeValue  + " or " + nodeDur.nodeValue +  "</div>";
     }else{
-        g_html += "<div class='well' id='step"+num+"'> " + nodeD.nodeValue +" "  +
+        g_html += "<div class='well' id='step"+num+"'>" + text  + " " + nodeD.nodeValue +" "  +
     nodeDist.nodeValue  + " or " + nodeDur.nodeValue +  "</div>";
     }
     
     panel.innerHTML = g_html;
+}
+
+function getModeImage(text) {
+    if (text.match(/DRIVING/i)) {
+        return "<i class='fa fa-car' aria-hidden='true'></i>";
+    } else if (text.match(/WALKING/i)) {
+        return "<i class='fa fa-male' aria-hidden='true'></i>";
+    } else if (text.match(/TRANSIT/i)) {
+        return "<i class='fa fa-bus' aria-hidden='true'></i>";
+    }
 }
